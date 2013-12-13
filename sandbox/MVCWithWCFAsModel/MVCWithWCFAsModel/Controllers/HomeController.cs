b9a726d21b89fbcc7164d3240c966ae190379497
Luis Teixeira <teixeira13@gmail.com>
2013-12-13 18:34:04 +0000

    Prova de conceito MVC 5 com wcf como fonte de dados close #6

---
+++ sandbox/MVCWithWCFAsModel/MVCWithWCFAsModel/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using MVCWithWCFAsModel.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithWCFAsModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        ServiceReference1.Service1Client ServiceProxy = new ServiceReference1.Service1Client();
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            List<Book> list = JsonConvert.DeserializeObject<List<Book>>(ServiceProxy.getProductsCatalog());
+            return View(list);
+        }
+	}
+}
---
+++ sandbox/MVCWithWCFAsModel/MVCWithWCFAsModel/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithWCFAsModel.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
