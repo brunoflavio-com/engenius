d3ebb230b9ab086562f1cdd4a71bb0e4cf7da9ad
Luis Teixeira <teixeira13@gmail.com>
2013-12-13 22:05:04 +0000

    Alteração do nome do serviço

--- SocialGameBLL/SocialGameBLL/SocialGameBLLService.svc.cs
+++ SocialGameBLL/SocialGameBLL/SocialGameBLLService.svc.cs
@@ -1,4 +1,5 @@
-﻿using SocialGameBLL.Entities;
+﻿using SocialGameBLL.Controllers;
+using SocialGameBLL.Entities;
 using SocialGameBLL.Security;
 using SocialGameBLL.Service;
 using System;
@@ -13,7 +14,7 @@ namespace SocialGameBLL
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class SocialGameBLLService : ISocialGameBLLService
     {
         public string GetData(int value)
         {
@@ -35,35 +36,26 @@ namespace SocialGameBLL
 
         public User RegisterUser(string Email, string Password)
         {
-            SocialGameBLLDbContext db = new SocialGameBLLDbContext();
-            UserEntity UserEntity = new UserEntity
+            try
             {
-                Email = Email,
-                HumourStatusID = 1
-            };
-
-            db.Users.Add(UserEntity);
-            db.SaveChanges();
-
-            SecurityService.RegisterUser(UserEntity, Password);
-            
-            return new User
+                AuthenticationController Controller = new AuthenticationController();
+                return Controller.RegisterUser(Email, Password);
+            }
+            catch (Exception e)
             {
-                Email = UserEntity.Email
-            };
+                throw new FaultException(e.Message);
+            }
         }
 
         public User LoginUser(string Email, string Password)
         {
-            SocialGameBLLDbContext db = new SocialGameBLLDbContext();
-            User User = new User();
-
-            if (SecurityService.LoginUser(Email, Password))
+            try{
+                AuthenticationController Controller = new AuthenticationController();
+                return Controller.LoginUser(Email, Password);
+            }catch(Exception e)
             {
-                UserEntity UserEntity = db.Users.Find(Email);
-                User.Email = UserEntity.Email;
+                throw new FaultException(e.Message);
             }
-            return User;
         }
     }
 }
