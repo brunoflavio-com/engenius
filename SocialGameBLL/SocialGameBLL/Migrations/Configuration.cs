1e0536f1e79772c1cb9091597e86278caa786694
Luis Teixeira <teixeira13@gmail.com>
2013-12-19 23:09:40 +0000

    Alteração ao metodo Seed para aumentar o numero de utilizadores e relações para efeitos de teste. #13

--- SocialGameBLL/SocialGameBLL/Migrations/Configuration.cs
+++ SocialGameBLL/SocialGameBLL/Migrations/Configuration.cs
@@ -54,7 +54,7 @@ namespace SocialGameBLL.Migrations
 
             var emails = new List<string>
             {
-                "test@test.com","test2@test.com","test3@test.com","test4@test.com"
+                "test@test.com","test2@test.com","test3@test.com","test4@test.com","test5@test.com","test6@test.com","test7@test.com","test8@test.com","test9@test.com"
             };
 
             var users = new List<UserEntity>(emails.Count);
@@ -102,7 +102,43 @@ namespace SocialGameBLL.Migrations
                     FriendEmail = users[1].Email,
                     RelationTagID = 2,
                     Strength = 6
-                }
+                },
+                new RelationshipEntity{
+                    UserEmail = users[3].Email,
+                    FriendEmail = users[4].Email,
+                    RelationTagID = 2,
+                    Strength = 3
+                },
+                new RelationshipEntity{
+                    UserEmail = users[1].Email,
+                    FriendEmail = users[5].Email,
+                    RelationTagID = 1,
+                    Strength = 1
+                },
+                new RelationshipEntity{
+                    UserEmail = users[2].Email,
+                    FriendEmail = users[6].Email,
+                    RelationTagID = 2,
+                    Strength = 3
+                },
+                new RelationshipEntity{
+                    UserEmail = users[2].Email,
+                    FriendEmail = users[8].Email,
+                    RelationTagID = 1,
+                    Strength = 4
+                },
+                new RelationshipEntity{
+                    UserEmail = users[0].Email,
+                    FriendEmail = users[7].Email,
+                    RelationTagID = 2,
+                    Strength = 8
+                },
+                new RelationshipEntity{
+                    UserEmail = users[0].Email,
+                    FriendEmail = users[6].Email,
+                    RelationTagID = 2,
+                    Strength = 4
+                },
             };
             relationships.ForEach(r => context.Relationships.AddOrUpdate(p => new { p.UserEmail, p.FriendEmail }, r));
             context.SaveChanges();
