f8e3a3caf6c413ca83454534acf404c31792f657
Luis Teixeira <teixeira13@gmail.com>
2013-12-19 23:04:30 +0000

    Inicio do projecto SocialGameWebSite. #16

---
+++ SocialGameWebSite/SocialGameWebSite/Authorization/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialGameWebSite.Authorization
+{
+    public class User : IUser
+    {
+
+        public bool IsAuthenticated
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string UserName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public System.Security.Principal.IIdentity Identity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SocialGameWebSite/SocialGameWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialGameWebSite.Startup))]
+namespace SocialGameWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
