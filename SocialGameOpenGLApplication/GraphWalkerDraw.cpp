39730c313282a61584ded9c4cf78807cbdcd6f0e
Bruno Flávio <bmflavio@gmail.com>
2013-12-19 20:31:48 +0000

    Correção no método GraphWalkerDraw::walkVertice: não é necessário o glbegin e o glend, visto que o glu os faz internamente. #32 @spikejap @LuisTeixeira @qsp

--- SocialGameOpenGLApplication/GraphWalkerDraw.cpp
+++ SocialGameOpenGLApplication/GraphWalkerDraw.cpp
@@ -36,28 +36,15 @@ void GraphWalkerDraw::walkConnection(User * userA, Relationship * relationship, 
 
 void GraphWalkerDraw::walkVertice(User * userA){
 
-	
-
-	
-
 	GLUquadricObj *quadric;
 	glPushMatrix();
 	glTranslatef(userA->x, userA->y, userA->z);
 
-	glBegin(GL_POLYGON);
 	quadric = gluNewQuadric();
 	gluQuadricDrawStyle(quadric, GLU_FILL);
 	gluSphere(quadric, 1, 30, 10);
 	glTranslatef(userA->x, userA->y, userA->z);
-	glColor3ub( rand()%255, rand()%255, rand()%255 );
-	//glTranslatef(2, 0, 2);
-	glEnd();
+	glColor3ub( rand()%255, rand()%255, rand()%255 );	
+
 	glPopMatrix();
-
-	//glColor3f(0, 0, 1);
-	//gluSphere(quadric, 1, 30, 10);
-	//glRotatef(-135, 0, 1, 0);
-	//gluCylinder(quadric, .5, .5, 2, 30, 10);
-
-	
 }
