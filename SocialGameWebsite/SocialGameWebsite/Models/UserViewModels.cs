167e3396f2033251e21f789ac7ec0ade83d04fc6
Luis Teixeira <teixeira13@gmail.com>
2013-12-29 00:25:09 +0000

    Inicio da construção do UserModelView. No momento deste commit ainda não foi feito o redeploy do serviço da BLL com os novos métodos, por isso erros de compilação são normais. #16

--- SocialGameWebsite/SocialGameWebsite/Models/UserViewModels.cs
+++ SocialGameWebsite/SocialGameWebsite/Models/UserViewModels.cs
@@ -7,6 +7,19 @@ namespace SocialGameWebsite.Models
 {
     public class User
     {
+        //public User(LocalHostSocialGameBLL.User BLLUser)
+        public User(SocialGameBLLService.User BLLUser)
+        {
+            this.Email = BLLUser.Email;
+            this.Name = BLLUser.Name;
+            this.Surname = BLLUser.Surname;
+            this.Birthdate = BLLUser.Birthdate;
+            this.PhoneNumber = BLLUser.PhoneNumber;
+            this.FacebookProfile = BLLUser.FacebookProfile;
+            this.LinkedinProfile = BLLUser.LinkedInProfile;
+            this.HumourStatus = GetHumourStatusFromService(BLLUser.HumourStatusId);
+        }
+
         public string Email { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
@@ -14,10 +27,23 @@ namespace SocialGameWebsite.Models
         public string PhoneNumber { get; set; }
         public string LinkedinProfile { get; set; }
         public string FacebookProfile { get; set; }
-        public HumorStatus HumorStatus { get; set; }
+        public HumourStatus HumourStatus { get; set; }
+
+        private HumourStatus GetHumourStatusFromService(int Id)
+        {
+            //LocalHostSocialGameBLL.SocialGameBLLServiceClient Proxy = new LocalHostSocialGameBLL.SocialGameBLLServiceClient();
+            //LocalHostSocialGameBLL.HumourStatus ServiceHumourStatus = Proxy.GetHumourStatus(Id);
+            SocialGameBLLService.SocialGameBLLServiceClient Proxy = new LocalHostSocialGameBLL.SocialGameBLLServiceClient();
+            SocialGameBLLService.HumourStatus ServiceHumourStatus = Proxy.GetHumourStatus(Id);
+            return new HumourStatus
+            {
+                ID = ServiceHumourStatus.Id,
+                Name = ServiceHumourStatus.Name
+            };
+        }
     }
 
-    public class HumorStatus
+    public class HumourStatus
     {
         public int ID { get; set; }
         public string Name { get; set; }
