7f15e30991f27e0c042cae8faac3a4a3b2772532
mmribeiro <m.agostinhoribeiro@hotmail.com>
2014-01-04 19:55:56 +0000

    Jogo 'hangman': Melhorias / correcao de bug (devolucao de vitoria) @mmribeiro

--- sandbox/MiniGamesAI/hangman_developing_file.pl
+++ sandbox/MiniGamesAI/hangman_developing_file.pl
@@ -31,6 +31,70 @@
 
 %(condicao1,condicao2,condicao3,((condicao4, condicao5,R is 0);R is 1));R is 2.
 
+%10 ?- string_to_list('abc',LY).
+%LY = [97, 98, 99].
+
+%11 ?- string_to_list(abc,LY).
+%LY = [97, 98, 99].
+
+%15 ?- string_chars(abc,L).
+%L = [a, b, c].
+
+%23 ?- char_code(a,L).
+%L = 97.
+
+
+%12 ?- word(W),string_chars(W,L).
+%W = abc,
+%L = [a, b, c].
+
+%13 ?- findall(U,used(U),LH), word(W),string_chars(W,L), intersection(LH,L,LR).
+%LH = [a, x, b, c],
+%W = abc,
+%L = LR, LR = [a, b, c].
+
+%14 ?- findall(U,used(U),LH), word(W),string_chars(W,L), intersection(LH,L,LR),compare(=,L,LR), write('YEAH!').
+%YEAH!
+%LH = [a, x, b, c],
+%W = abc,
+%L = LR, LR = [a, b, c].
+
+
+%1 ?- assert(word('abc')).
+%true.
+
+%2 ?- play('a',R).
+%R = 1.
+
+%3 ?- play('b',R).
+%R = 1.
+
+%4 ?- play('c',R).
+%R = 1.
+
+%5 ?- chekWin(R).
+%WIN!
+%R = 0. 
+
+%1 ?- assert(word('abc')).
+%true.
+
+%2 ?- play('b',R).
+%R = 1.
+
+%3 ?- chekWin(R).
+%false.
+
+%4 ?- play('a',R).
+%R = 1.
+
+%5 ?- play('c',R).
+%R = 1.
+
+%6 ?- chekWin(R).
+%WIN!
+%R = 0.
+
 
 %%%%%%%%%% TO USE PREDICATES %%%%%%%%%%
 
@@ -38,18 +102,31 @@
 :- dynamic used/1.
 
 %already used characters knowledge base example
-used(a).
-used(b).
-used(c).
-used(d).
-used(r).
+%used(a).
+%used(b).
+%used(c).
+%used(d).
+%used(r).
 
 %starting game cpp - assert(word(W)).
 %during game Prolog - assert(used(C)).
 
-find(X,Y,R):- ( char_code(X,LX), string_to_list(Y,LY), member(LX,LY), ((findall(U,used(U),R), compare(=,LY,R), R is 0 ); R is 1)); R is 2.
-
 play(C,R):- not(used(C)),
 			assert(used(C)),
 			!,
+			word(W),
 			find(C,W,R).
+			
+%find (character,word,Result)
+find(X,Y,R):- (string_chars(Y,LY),
+			member(X,LY),
+			((checkWin(R),!); R is 1,!)); R is 2.
+			
+checkWin(P):-  findall(U,used(U),LH),
+			word(W),
+			string_chars(W,L),
+			intersection(LH,L,LR),
+			%compare(=,L,LR),
+			same_length(L,LR),
+			P is 0,
+			write('WIN!').
