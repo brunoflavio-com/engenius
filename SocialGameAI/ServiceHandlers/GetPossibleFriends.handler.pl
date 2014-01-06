222be0e9d578a3ef1d97b85e5d7594ad5392f04d
Jorge Pinto <spikejap@gmail.com>
2014-01-06 23:46:23 +0000

    Primeira versão final do handler. #23

--- SocialGameAI/ServiceHandlers/GetPossibleFriends.handler.pl
+++ SocialGameAI/ServiceHandlers/GetPossibleFriends.handler.pl
@@ -34,6 +34,32 @@ get_possible_friends(Request) :-
 %%
 % Compute:
 get_possible_friends_action(UserEmail, SugestedFriends):-
+	findall((Common,UserB),
+		(http_session_data(user(UserB,_,_,_,_,_,_,_,_)),
+			UserB\=UserEmail,
+			not(http_session_data(relationship(UserEmail,UserB,_,_))),
+			not(http_session_data(relationship(UserB,UserEmail,_,_))),
+			write(UserB),nl,
+			get_common_interests(UserEmail,UserB, Common)),
+		List),
+	sort(List, List1),
+	reverse(List1,List2),
+	findall(User,
+		member((_,User),List2),
+		SugestedFriends).		
+
+get_common_interests(UserA,UserB, Common):-
+	http_session_data(user(UserA,_,_,_,_,InterestsA,_,_,_)),
+	http_session_data(user(UserB,_,_,_,_,InterestsB,_,_,_)),
+	get_common_interests_action(InterestsA, InterestsB, Common),!.
+	
+get_common_interests_action([], _, 0).
+get_common_interests_action([H|T], InterestsB, Common):-
+	member(H,InterestsB),
+	get_common_interests_action(T,InterestsB,Common1),
+	Common is Common1+1.
+get_common_interests_action([_|T], InterestsB, Common):-
+	get_common_interests_action(T,InterestsB,Common).
 
 
 
