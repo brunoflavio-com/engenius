ae66a6ea7226a799447290c517f8e5ba4c8c840f
Jorge Pinto <spikejap@gmail.com>
2014-01-06 20:50:14 +0000

    Criação do handler para obter estatisticas de um determinado grafo. #21

---
+++ SocialGameAI/ServiceHandlers/GetGraphStats.handler.pl
@@ -0,0 +1,17 @@
+%
+% FILE: GetGraphStats.handler.pl
+%
+
+%
+% Social Game Server: Handler for Graph stats
+%
+
+%%
+%Libraries:
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_parameters)).
+:- use_module(library(http/http_session)). 
+:- use_module(library(http/http_json)).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+:- use_module(library(http/http_error)).
