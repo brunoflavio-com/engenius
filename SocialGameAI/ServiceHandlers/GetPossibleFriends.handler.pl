2c78e8b3f84ed0c0bd0019bc7e145aa030a5f601
Jorge Pinto <spikejap@gmail.com>
2014-01-06 22:31:45 +0000

    Criado Request Handler. #23

--- SocialGameAI/ServiceHandlers/GetPossibleFriends.handler.pl
+++ SocialGameAI/ServiceHandlers/GetPossibleFriends.handler.pl
@@ -15,3 +15,27 @@
 :- use_module(library(http/json)).
 :- use_module(library(http/json_convert)).
 :- use_module(library(http/http_error)).
+
+%%
+% Register this handler on the server:
+:-http_handler('/getpossiblefriends', get_possible_friends, []).
+
+%%
+% Request Handler:
+get_possible_friends(Request) :-
+	http_parameters(Request,
+                    [ 
+					useremail(UserEmail, [])
+					]),
+	get_possible_friends_action(UserEmail, SugestedFriends),
+	prolog_to_json(SugestedFriends, SugestedFriendsJSON),
+	reply_json(SugestedFriendsJSON).
+
+%%
+% Compute:
+get_possible_friends_action(UserEmail, SugestedFriends):-
+
+
+
+
+
