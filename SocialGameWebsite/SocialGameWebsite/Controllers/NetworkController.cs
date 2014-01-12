1bbd09e356d9e4a59f2bb9d094648c9348d3cd96
Bruno Flávio <bmflavio@gmail.com>
2014-01-12 23:52:44 +0000

    Site apresenta o tamanho da rede global/utilizador e média da força de ligações #25

--- SocialGameWebsite/SocialGameWebsite/Controllers/NetworkController.cs
+++ SocialGameWebsite/SocialGameWebsite/Controllers/NetworkController.cs
@@ -36,42 +36,72 @@ namespace SocialGameWebsite.Controllers
         }
 
 
-        public ActionResult InterestsTagCloud(bool global = false)
+        public ActionResult InterestsGlobalTagCloud()
         {
             TagCloudViewModel ViewModel = new TagCloudViewModel();
 
-            if (global)
-            {
-                ViewModel.Title = "Global interests cloud:";
-                ViewModel.Data = Proxy.GetInterestsTagCloud(null);
-            }
-            else
-            {
-                ViewModel.Title = "User interests cloud:";
-                ViewModel.Data = Proxy.GetInterestsTagCloud(new User { Email = User.Identity.Name });
-            }
+            ViewModel.Title = "Global interests cloud:";
+            ViewModel.Data = Proxy.GetInterestsTagCloud(null);
             
-
             return PartialView("TagCloud", ViewModel);
         }
 
-        public ActionResult RelationshipsTagCloud(bool global = false)
+        [Authorize]
+        public ActionResult InterestsUserTagCloud()
         {
             TagCloudViewModel ViewModel = new TagCloudViewModel();
 
-            if (global)
-            {
-                ViewModel.Title = "Global relationships cloud:";
-                ViewModel.Data = Proxy.GetRelationshipsTagCloud(null);
-            }
-            else
-            {
-                ViewModel.Title = "User relationships cloud:";
-                ViewModel.Data = Proxy.GetRelationshipsTagCloud(new User { Email = User.Identity.Name });
-            }
+            ViewModel.Title = "User interests cloud:";
+            ViewModel.Data = Proxy.GetInterestsTagCloud(new User { Email = User.Identity.Name });
+    
+            return PartialView("TagCloud", ViewModel);
+        }
 
+        public ActionResult RelationshipsGlobalTagCloud()
+        {
+            TagCloudViewModel ViewModel = new TagCloudViewModel();
+
+            ViewModel.Title = "Global relationships cloud:";
+            ViewModel.Data = Proxy.GetRelationshipsTagCloud(null);
 
             return PartialView("TagCloud", ViewModel);
+        }
+        
+        [Authorize]
+        public ActionResult RelationshipsUserTagCloud()
+        {
+            TagCloudViewModel ViewModel = new TagCloudViewModel();
+
+            ViewModel.Title = "User relationships cloud:";
+            ViewModel.Data = Proxy.GetRelationshipsTagCloud(new User { Email = User.Identity.Name });
+            
+            return PartialView("TagCloud", ViewModel);
+        }
+
+        public ActionResult NetworkGlobalStatistics()
+        {
+            StatisticsViewModel ViewModel = new StatisticsViewModel();
+
+            NetworkStatistics NetworkStatistics = Proxy.GetGraphStats(null);
+            
+            ViewModel.Title = "Global network statistics:";
+            ViewModel.UserCount = NetworkStatistics.UserCount;
+            ViewModel.RelationshipStrenghtAverage = NetworkStatistics.RelationshipStrenghtAverage;
+            
+            return PartialView("Statistics", ViewModel);
+        }
+
+        public ActionResult NetworkUserStatistics()
+        {
+            StatisticsViewModel ViewModel = new StatisticsViewModel();
+
+            NetworkStatistics NetworkStatistics = Proxy.GetGraphStats(new User { Email = User.Identity.Name });
+            
+            ViewModel.Title = "User network statistics:";
+            ViewModel.UserCount = NetworkStatistics.UserCount;
+            ViewModel.RelationshipStrenghtAverage = NetworkStatistics.RelationshipStrenghtAverage;
+
+            return PartialView("Statistics", ViewModel);
         }
     }
 }
